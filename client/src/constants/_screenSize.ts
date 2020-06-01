1e02fc1013bd56c774296356441d8544c74c42b4
Mark <mvincha404@gmail.com>
2020-06-01 20:47:23 +0300

    client(feat): added screen size constants

---
+++ client/src/constants/_screenSize.ts
@@ -0,0 +1,6 @@
+export enum screenSize {
+  PC = "1920px",
+  MIN_PC = "1280px",
+  LAPTOP = "768px",
+  MOBILE = "480px",
+}
--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,3 +1,4 @@
 export * from "./_colors";
 export * from "./_dimens";
 export * from "./_url";
+export * from "./_screenSize";
